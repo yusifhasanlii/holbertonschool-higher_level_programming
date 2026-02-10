6179ae9b628a4beda1728edd3c199d834d7141e7
yusifhasanlii <hesenli.2007.y@gmail.com>
2026-02-10 15:17:36 +0000

    task 7 done

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = (tuple_a + (0, 0)[:2]
+    tuple_b = (tuple_b + (0, 0)[:2]
+    result_1 = tuple_a[0] + tuple_b[0]
+    result_2 = tuple_a[1] + tuple_b[1]
+    return (result_1, result_2)
