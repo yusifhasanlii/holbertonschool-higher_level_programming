f50f8d45ba8e4c505ea5e776709a971847b6c0f1
yusifhasanlii <hesenli.2007.y@gmail.com>
2026-02-11 19:13:36 +0000

    advanced task 1 done

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,22 +2,22 @@
 from sys import argv, exit
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    if not len(sys.argv) == 4:
+    if not len(argv) == 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if sys.argv[2] == "+":
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-        sys.exit(0)
-    elif sys.argv[2] == "-":
+        exit(0)
+    elif argv[2] == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
         sys.exit(0)
-    elif sys.argv[2] == "*":
+    elif argv[2] == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
         sys.exit(0)
-    elif sys.argv[2] == "/":
+    elif argv[2] == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
