71bd85aa361ba4ea0a93c701b3e0189085e96253
yusifhasanlii <hesenli.2007.y@gmail.com>
2026-02-11 19:11:10 +0000

    advanced task 1 done

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import sys
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    if len(sys.argv) > 4:
+    if not len(sys.argv) == 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
-    b = int(sys.argv[2])
+    b = int(sys.argv[3])
     if sys.argv[2] == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
         sys.exit(0)
