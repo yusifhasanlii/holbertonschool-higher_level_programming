360d0558824fb9d7cccda8a6279cb202e96166fd
yusifhasanlii <hesenli.2007.y@gmail.com>
2026-02-17 21:04:26 +0000

    task 6 done

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for key in a_dictionary:
-        print("{}: {}".fotmat(key, a_dictionary[key]))
+        print("{}: {}".format(key, a_dictionary[key]))
