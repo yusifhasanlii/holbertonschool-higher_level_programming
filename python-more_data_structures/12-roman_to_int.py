980476658552ba7afab93db8090a9fd69d7ca3cc
yusifhasanlii <hesenli.2007.y@gmail.com>
2026-02-18 19:07:01 +0000

    task 12 done

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 100
+    }
+    total = 0
+    length = len(roman_string)
+    for i in range(length):
+        current_val = roman_dict[roman_string[i]]
+        if i < length - 1 and current_val < roman_dict[roman_string[i+1]]:
+            total -= current_val
+        else:
+            total += current_val
+    return total
