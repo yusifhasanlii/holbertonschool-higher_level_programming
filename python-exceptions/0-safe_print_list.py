d04ca2d55ab984873e042d15148d9e1036b2a6d4
yusifhasanlii <Hesenli.2007.y@gmail.com>
2026-02-20 10:01:14 +0000

    task 0 done

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list[0], x=0):
         try:
             print(my_list[i], end="")
             count += 1
-        except:
+        except IndexError:
             break
     print()
     return count
