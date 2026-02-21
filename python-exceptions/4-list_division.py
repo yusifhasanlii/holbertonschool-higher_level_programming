335b5b6978887602133a7992711be8bcbaf1a145
yusifhasanlii <Hesenli.2007.y@gmail.com>
2026-02-21 12:37:39 +0000

    task 4 done

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         res = 0
         try:
-            res = my_list[i] / my_list[i]
+            res = my_list_1[i] / my_list_2[i]
         except ValueError:
             res = 0
         except TypeError:
