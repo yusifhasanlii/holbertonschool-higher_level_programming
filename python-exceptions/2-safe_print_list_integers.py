7a81ae8f492fe0dd0e51a21244897cf6293eb125
yusifhasanlii <Hesenli.2007.y@gmail.com>
2026-02-21 11:50:25 +0000

    task 2 done

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    for i in range(x):
+        count = 0
+        try:
+            print("{:d}".format(my_list[i], end=""))
+            count += 1
+        except (TypeError, ValueError):
+            contunie
+    print()
+    return count
