de687c5374091569703b9a0c6ca544ce07148a6b
yusifhasanlii <Hesenli.2007.y@gmail.com>
2026-02-27 10:27:09 +0000

    task 1 done

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""This module defines a Square class"""
+
+
 class Square:
+    """A class that defines a square."""
+
     def __init__(self, size):
+        """Initalizes a new Square.
+
+        Args:
+            size: The size of the new square.
+        """
         self.__size = self
