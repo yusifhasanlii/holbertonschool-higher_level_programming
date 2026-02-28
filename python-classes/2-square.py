b0303c6bfa887186f90c6139e6ec9b60e3150abb
yusifhasanlii <Hesenli.2007.y@gmail.com>
2026-02-28 11:14:13 +0000

    task 2 done

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        if isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be integer")
         if size < 0:
             raise ValueError("size must be >= 0")
