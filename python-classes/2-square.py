b522f0f1d5f2d01dede23432deef543d96a8539e
yusifhasanlii <Hesenli.2007.y@gmail.com>
2026-02-28 11:12:20 +0000

    task 2 done

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Define a class square."""
+"""Define a class Square."""
 
 
 class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square
+        """Initialize a new Square.
 
         Args:
-            size (int): The size of new square.
+            size (int): The size of the new square.
         """
         if type(size) is not int:
             raise TypeError("size must be integer")
