acbd4a3fcb7308902f55b9f380ee485d55071e88
yusifhasanlii <Hesenli.2007.y@gmail.com>
2026-03-22 17:11:17 +0000

    task 4 done

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Lists all cities from the database hbtn_0e_4_usa.
+Sorted by cities.id in ascending order.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name "
+                   "FROM cities "
+                   "INNER JOIN states ON cities.state_id = states.id "
+                   "ORDER BY cities.id ASC")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
