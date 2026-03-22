60584349f7a2a076f050dc2a1287878338305663
yusifhasanlii <Hesenli.2007.y@gmail.com>
2026-03-22 17:20:49 +0000

    task 10 done

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -10,23 +10,23 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    # Engine yaradılır
+    # Create the engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    # Session yaradılır
+    # Create the session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query: Şərtdə verilən ada görə axtarış
-    # .first() istifadə edirik çünki bir nəticə gözləyirik
+    # Query: Search by the name provided as an argument
+    # Using .first() because we expect one result
     state = session.query(State).filter(State.name == sys.argv[4]).first()
 
-    # Nəticə varsa ID-ni çap edirik, yoxsa "Not found"
+    # If the state exists, print its ID, otherwise print "Not found"
     if state:
         print("{}".format(state.id))
     else:
         print("Not found")
 
-    # Session bağlanır
+    # Close the session
     session.close()
