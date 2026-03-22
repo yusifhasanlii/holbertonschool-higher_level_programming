08e0f1155d33044325c897dcfd5419f4da69d20f
yusifhasanlii <Hesenli.2007.y@gmail.com>
2026-03-22 17:14:42 +0000

    task 7 done

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Lists all State objects from the database hbtn_0e_6_usa.
+Using SQLAlchemy to fetch data.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    # Engine yaradılır
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Session yaradılır
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query: Bütün ştatları id üzrə artan sıra ilə götürürük
+    states = session.query(State).order_by(State.id).all()
+
+    # Nəticələri tələb olunan formatda çap edirik
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    # Session bağlanır
+    session.close()
