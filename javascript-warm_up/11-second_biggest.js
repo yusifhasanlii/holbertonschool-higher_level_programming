94110528714b0a4c9c3012ce5f865e96ea5cced4
yusifhasanlii <Hesenli.2007.y@gmail.com>
2026-03-29 16:59:44 +0000

    task 11 done

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.slice(2).map(Number);
+  const uniqueSorted = args.sort((a, b) => b - a);
+  console.log(uniqueSorted[1]);
+}                                                                                                                                                                                                                                
+~                                                                                                                                       
+~                                                                                                                                       
