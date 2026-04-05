c434eaa70bb116c4115c4e4a9e0749583bfaafe4
yusifhasanlii <Hesenli.2007.y@gmail.com>
2026-04-05 10:54:14 +0000

    task 3 done

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,49 @@
+import json
+import csv
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def read_json():
+    with open('products.json', 'r') as f:
+        return json.load(f)
+
+def read_csv():
+    products = []
+    with open('products.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            row['id'] = int(row['id'])
+            row['price'] = float(row['price'])
+            products.append(row)
+    return products
+
+@app.route('/products')
+def display_products():
+    source = request.args.get('source')
+    product_id = request.args.get('id')
+    products = []
+    error = None
+
+    # Source yoxlanışı
+    if source == 'json':
+        products = read_json()
+    elif source == 'csv':
+        products = read_csv()
+    else:
+        return render_template('product_display.html', error="Wrong source")
+
+    # ID filtrləmə (əgər URL-də id varsa)
+    if product_id:
+        try:
+            target_id = int(product_id)
+            products = [p for p in products if p['id'] == target_id]
+            if not products:
+                error = "Product not found"
+        except ValueError:
+            error = "Invalid ID format"
+
+    return render_template('product_display.html', products=products, error=error)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
